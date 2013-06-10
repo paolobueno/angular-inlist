5e2a3e926a494c20eec5ec20f071e9eb3fcac39a
Paolo Bueno <paolohaji@gmail.com>
2013-06-10 09:15:13 -0300

    remove underscore dependency

--- angular-inlist.js
+++ angular-inlist.js
@@ -1,12 +1,17 @@
 angular.module('inlist', [])
 .directive('inList', function () {
+    // lets 
+    function contains (arr, value) {
+        if (arr === null) return false;
+        return arr.indexOf(value) != -1;
+    }
     return {
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, el, attrs, ctrl) {
             if (attrs.inList) {
                 ctrl.$parsers.unshift(function (viewValue) {
-                    var valid = _.contains(scope.$eval(attrs.inList), viewValue);
+                    var valid = contains(scope.$eval(attrs.inList), viewValue);
                     ctrl.$setValidity('inList', valid);
 
                     return valid ? viewValue : undefined;
